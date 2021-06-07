a36732155b2ba6614a2c889bb58dac64b060befd
Evandro Avellar <evandro.avellar@gmail.com>
2021-06-07 17:05:17 -0300

    Primeiros arquivos em R

---
+++ arquivo_inicial.R
@@ -0,0 +1,22 @@
+# Script de teste
+
+# Configurando e consultando o diretório de trabalho
+setwd("~/Programacao/R")
+getwd()
+
+# Chamando a função que traz o licenciamento
+license()
+
+# Utilizando o print()
+print("Introdução à linguagem R")
+
+# Como instalar um pacote
+# install.packages('caret')
+
+# Como chamar um pacote instalado
+library(caret)
+
+help(setwd)
+?library
+
+
---
+++ operadores.R
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+
+((8+7) * 4 + (120 / 40)^2) %% 13
+
+
+# Operadores relacionais
+## Atribuição
+x = 6
+y <- 10
+## Relacionais
+x > 5
+y < x
+y != x
+## Lógicos
+(y == 10)&(x == 6)
+(y == 10)&(x == 16)
+(y == 10)|(x == 16)
+(y >= 10)|(x < 6)
+
+## not
+x > 6
+print(!x>6)
